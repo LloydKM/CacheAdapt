c911dcc0993a0d4d63f1f489dd845211a29227ad
Benedict Hans <benedict.hans@web.de>
2022-05-24 23:22:46 +0200

    first steps creating caching layer with klibs hashtable. Bearbone Code and TODOs

--- src/cache_layer.c
+++ src/cache_layer.c
@@ -5,20 +5,60 @@ system calls.
 #define _POSIX_C_SOURCE 200809L
 #define _GNU_SOURCE
 
-/* TODO:
-
-Do I want other scripts to be able to access these? 
-Option 1: Specific access through function calls
-Option 2: private access only in this script
-Option 3: Open access through header
-*/
-// Hard coded cache_path as default- -> Does this incur Option 3?
-static const char *cache_path = "/tmp/CacheAdapt";
+#include "cache_layer.h"
 
 /* TODO:
 
 What functions do I need?
-- something to store file
-- something to access file
+- something to initialize layer
+- something to store/check file
+- something to load adjacent files
+- something to access file ????
 - something to create link or hash name (like a file handle)
 */
+
+int
+init_layer(const char *path)
+{
+    khint_t iterator;
+    // allocate a hash table
+    h = kh_init(m32);
+    return 0;
+}
+
+int
+check_layer(const char *path)
+{
+    int ret;
+    bool is_missing;
+    khint_t iterator;
+    /* TODO:
+
+    - return hash key?
+    */
+    if (!is_initialized)
+    {
+        ret = init_layer(path);
+
+        if (ret < 0)
+        {
+            perror("Couldn't initialize layer");
+            return -1;
+        }
+    }
+
+    // check for match in table
+    iterator = kh_get(m32, h, path);
+    is_missing = (iterator == kh_end(h));
+
+    if(!is_missing) 
+    {
+        // TODO: if entry exists
+    }
+    else
+    {
+        // TODO: if entry is missing
+    }
+
+    return 0;
+}
--- src/cache_layer.h
+++ src/cache_layer.h
@@ -1,4 +1,26 @@
 #ifndef CACHE_LAYER_H
 #define CACHE_LAYER_H
 
+#include <stdbool.h>
+#include <stdio.h>
+
+// Don't forget include path for compilation -Ilib
+#include "klib/khash.h"
+
+KHASH_MAP_INIT_STR(m32, const char *)
+/* TODO:
+
+Do I want other scripts to be able to access these? 
+Option 1: Specific access through function calls
+Option 2: private access only in this script
+Option 3: Open access through header
+*/
+// Hard coded cache_path as default- -> Does this incur Option 3?
+static const char *cache_path = "/tmp/CacheAdapt";
+static bool is_initialized = false;
+static khash_t(m32) *h;
+
+int init_layer(const char *path);
+int check_layer(const char *path);
+
 #endif
